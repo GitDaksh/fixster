cb50f54f573d88bdfd4aebd27c5596abac80ab76
Daksh <daksh.java.util@gmail.com>
2025-03-19 18:57:16 +0530

    auth using clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,15 @@
 import { Metadata } from "next";
 import "./globals.css";
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
 export const metadata: Metadata = {
   title: "Fixster - AI Code Debugger",
   description: "A powerful AI-driven code debugging tool",
@@ -13,12 +22,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body>
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
         <div className="flex min-h-screen bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100">
           {children}
         </div>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
